bffb82f9e3f12e02c27dde711753778642c6e368
exequielrafaela <exequiel.barrirero@binbash.com.ar>
2019-04-10 09:58:44 -0300

    BBL-49 AWS Security Group terraform module initial commit

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+# SG attributes
+output "id" {
+  description = "The ID of the security group"
+  value = "${aws_security_group.default.id}"
+}
+output "arn" {
+  description = "The ARN of the security group"
+  value = "${aws_security_group.default.arn}"
+}
+output "owner_id" {
+  description = "The owner ID of the security group"
+  value = "${aws_security_group.default.owner_id}"
+}
+output "name" {
+  description = "The name of the security group"
+  value = "${aws_security_group.default.name}"
+}
+
+# SG Vpc id
+output "vpc_id" {
+  description = "Sec group VPC id"
+  value = "${aws_security_group.default.vpc_id}"
+}
+
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "tcp_ports" {
+  description = "A comma separated list of TCP ports from 1-65536 eg: 22,80,443"
+  default = "default_null"
+}
+
+variable "udp_ports" {
+  description = "A comma separated list of UDP ports from 1-65536 eg: 5001,5002,5003"
+  default = "default_null"
+}
+
+variable "cidrs" {
+  description = "A comma separated list of CIDR segmemnts eg: [172.16.0.0/20,0.0.0.0/0]"
+  type = "list"
+}
+
+variable "security_group_name" {
+    description = "The security group name"
+}
+
+variable "vpc_id" {
+    description = "The VPC id to create the security group in"
+}
+
+variable "tags" {
+  type = "map"
+  description = "A mapping of tags to assign to all resources"
+  default     = {}
+}
